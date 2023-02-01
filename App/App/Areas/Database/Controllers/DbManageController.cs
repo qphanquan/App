2676568e1c4bee2135e9137931976fd65277d0a5
qphanquan <116238009+qphanquan@users.noreply.github.com>
2023-02-02 01:27:21 +0700

    up

---
+++ App/App/Areas/Database/Controllers/DbManageController.cs
@@ -0,0 +1,86 @@
+﻿using App.Data;
+using App.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.Areas.Database.Controllers
+{
+    [Area("Database")]
+    [Route("/database-manage/[action]")]
+    public class DbManageController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+        private readonly UserManager<AppUser> _userManager;
+
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public DbManageController(AppDbContext dbContext, UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager, SignInManager<AppUser> signInManager)
+        {
+            _dbContext = dbContext;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> SeedDataAsync()
+        {
+            // Create Roles
+            var rolenames = typeof(RoleName).GetFields().ToList();
+            foreach (var r in rolenames)
+            {
+                var rolename = (string)r.GetRawConstantValue();
+                var rfound = await _roleManager.FindByNameAsync(rolename);
+                if (rfound == null)
+                {
+                    await _roleManager.CreateAsync(new IdentityRole(rolename));
+                }
+            }
+
+            // admin, pass=admin123, admin@example.com
+            var useradmin = await _userManager.FindByEmailAsync("admin@example.com");
+            if (useradmin == null)
+            {
+                useradmin = new AppUser()
+                {
+                    UserName = "admin",
+                    Email = "admin@example.com",
+                    EmailConfirmed = true,
+                };
+
+                await _userManager.CreateAsync(useradmin, "admin123");
+                await _userManager.AddToRoleAsync(useradmin, RoleName.Administrator);
+                await _signInManager.SignInAsync(useradmin, false);
+
+                return RedirectToAction("SeedData");
+
+            }
+            else
+            {
+                var user = await _userManager.GetUserAsync(this.User);
+                if (user == null) return this.Forbid();
+                var roles = await _userManager.GetRolesAsync(user);
+
+                if (!roles.Any(r => r == RoleName.Administrator))
+                {
+                    return this.Forbid();
+                }
+
+            }
+
+
+
+            /*SeedPostCategory();
+            SeedProductCategory();
+
+            StatusMessage = "Vừa seed Database"*/;
+            return RedirectToAction("Index");
+
+
+        }
+    }
+}
